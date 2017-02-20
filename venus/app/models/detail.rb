89aec59228d0a9aff203cdec62bf754a24f84f9e
NataliaFaundez <natalia.faundez@ug.uchile.cl>
2017-02-20 19:54:57 -0300

    validaciones-falta

--- venus/app/models/detail.rb
+++ venus/app/models/detail.rb
@@ -1,4 +1,5 @@
 class Detail < ActiveRecord::Base
   belongs_to :service
   belongs_to :client
+
 end
--- venus/app/models/product.rb
+++ venus/app/models/product.rb
@@ -1,3 +1,16 @@
 class Product < ActiveRecord::Base
 	has_many :sales, dependent: :destroy
+  
+  # Validamos que el codigo, precio y stock solo sea numerico
+  validates :codigo_producto, numericality: { only_integer: true }
+  validates :codigo_producto, presence: true #dice que esta 
+  validates :nombre, presence: true #dice que esta 
+  validates :precio, numericality: { only_integer: true }
+  validates :stock, numericality: { only_integer: true }
+
+  # Validamos que el codigo de producto y producto sea unico
+  validates :codigo_producto, uniqueness: {case_sensitive: false ,message: "ya esta registrado"}
+  validates :nombre, uniqueness: {case_sensitive: false ,message: "ya esta registrado"}
+	
+	#el nombre del producto no tiene que ser solo letras, ya que hay productos con nombre de numeros o signos	
 end
