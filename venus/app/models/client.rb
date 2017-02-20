14725598d797c29fc02769fed3eb45f4d3057235
david jorquera <david_jorquera@hotmail.com>
2017-02-20 16:28:51 -0300

    algunas reparaciones a la base de datos

--- venus/app/models/client.rb
+++ venus/app/models/client.rb
@@ -1,2 +1,4 @@
 class Client < ActiveRecord::Base
+	has_many :sale, dependent: :destroy
+	has_many :detail, dependent: :destroy
 end
--- venus/app/models/service.rb
+++ venus/app/models/service.rb
@@ -1,3 +1,4 @@
 class Service < ActiveRecord::Base
   belongs_to :stylist
+  has_many :detail, dependent: :destroy
 end
