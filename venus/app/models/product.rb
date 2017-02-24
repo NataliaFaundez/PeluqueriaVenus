f9b657d01d8b8dfcc0503f05b0a02d6c2a884b3a
NataliaFaundez <natalia.faundez@ug.uchile.cl>
2017-02-24 00:21:46 -0300

    validacion de numeros positivos, falta probar

--- venus/app/models/product.rb
+++ venus/app/models/product.rb
@@ -5,8 +5,12 @@ class Product < ActiveRecord::Base
   validates :codigo_producto, numericality: { only_integer: true }
   validates :codigo_producto, presence: true #dice que esta 
   validates :nombre, presence: true #dice que esta 
-  validates :precio, numericality: { only_integer: true }
+  #validates :precio, numericality: { only_integer: true }
+  #se supone que es para los numeros positivos si funciona se elimina la de arriba 
+  #y se deja la de abajo y se reemplaza todas las que sean numericas y si no queda la de arriba
+  validates :precio, numericality: { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100 }
   validates :stock, numericality: { only_integer: true }
+#falta de numeros positivos
 
   # Validamos que el codigo de producto y producto sea unico
   validates :codigo_producto, uniqueness: {case_sensitive: false ,message: "ya esta registrado"}
