1f93ddf607a5eec1267027bf7fab50565dd51d39
NataliaFaundez <natalia.faundez@ug.uchile.cl>
2017-02-24 01:12:07 -0300

    validaciones, tengo dos propuestas para que el numero sea positivo una con tope y una sin tope, tambien tengo una cosa de confirmacion pero no estoy segura

--- venus/app/models/client.rb
+++ venus/app/models/client.rb
@@ -4,9 +4,9 @@ class Client < ActiveRecord::Base
 	validates_associated :sales
 	validates_associated :details
 
-	validates :rut , presence: true 
+	validates :rut , :presence => {:message => "Usted debe ingresar un rut"}, length: {minimum: 8, maximum: 9, :message => "El rut debe tener entre 8 y 9 caracteres"}
 	validates :rut, uniqueness: {case_sensitive: false ,message: "ya esta registrado"}
-	validates :rut, length: { in: 8..9 }
+
 
 	validates :nombre, presence: true #dice que esta 
 	validates :nombre, length: { in: 6..30 }
--- venus/app/models/product.rb
+++ venus/app/models/product.rb
@@ -4,17 +4,19 @@ class Product < ActiveRecord::Base
   
   # Validamos que el codigo, precio y stock solo sea numerico
   validates :codigo_producto, numericality: { only_integer: true }
-  validates :codigo_producto, presence: true #dice que esta 
+  validates :codigo_producto, :presence => {:message => "Usted debe ingresar un codigo"}, length: {minimum: 2, maximum: 50, :message => "El codigo debe tener entre 2 y 50 caracteres"}
+
   # Validamos que el codigo de producto y producto sea unico
   validates :codigo_producto, uniqueness: {case_sensitive: false ,message: "ya esta registrado"}
 
-  validates :nombre, presence: true #dice que esta 
+  validates :nombre, :presence => {:message => "Usted debe ingresar un nombre"}, length: {minimum: 2, maximum: 50, :message => "El nombre debe tener entre 2 y 50 caracteres"}
   validates :nombre, length: { in: 3..20 }
   validates :nombre, uniqueness: {case_sensitive: false ,message: "ya esta registrado"}
-  #validates :precio, numericality: { only_integer: true }
+  
+  #validates :precio, :numericality => { :greater_than_or_equal_to => 0 }
   #se supone que es para los numeros positivos si funciona se elimina la de arriba 
   #y se deja la de abajo y se reemplaza todas las que sean numericas y si no queda la de arriba
-  validates :precio, numericality: { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100 }
+  validates :precio, numericality: => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100 }
   validates :stock, numericality: { only_integer: true }
 	#el nombre del producto no tiene que ser solo letras, ya que hay productos con nombre de numeros o signos	
 end
