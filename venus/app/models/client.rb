08e9afd81e0c48799941dc08f076b4650f9c9079
NataliaFaundez <natalia.faundez@ug.uchile.cl>
2017-02-24 00:32:43 -0300

    validaciones de clientes

--- venus/app/models/client.rb
+++ venus/app/models/client.rb
@@ -1,4 +1,12 @@
 class Client < ActiveRecord::Base
 	has_many :sales, dependent: :destroy
 	has_many :details, dependent: :destroy
+
+	validates :rut , presence: true 
+	validates :rut, uniqueness: {case_sensitive: false ,message: "ya esta registrado"}
+
+	validates :nombre, presence: true #dice que esta 
+	validates :nombre, uniqueness: {case_sensitive: false ,message: "ya esta registrado"}
+	#falta de numeros positivos
+	validates :telefono, numericality: { only_integer: true }	
 end
