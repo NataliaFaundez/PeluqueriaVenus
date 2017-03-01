360d7827fe5cc54664699eea3d2c5d417abb5994
david jorquera <david_jorquera@hotmail.com>
2017-02-28 21:55:18 -0300

    ruta y metodo para eliminar usuarios

--- venus/app/controllers/user_controller.rb
+++ venus/app/controllers/user_controller.rb
@@ -3,5 +3,10 @@ class UserController < ApplicationController
   	@users = User.all
   end
 
+  def destroy
+  	@user = User.find(params[:id])
+  	@user.destroy
+  end
+
   
 end
--- venus/config/routes.rb
+++ venus/config/routes.rb
@@ -28,6 +28,7 @@ Rails.application.routes.draw do
   get 'contador' => 'contador#index'
   get 'nuevo_producto' => 'products#new'
   get 'usuarios' => 'user#index'
+  delete 'usuarios' => 'user#destroy'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
